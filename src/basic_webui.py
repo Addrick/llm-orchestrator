3fc9d69dd1783206dd8d8ad0afe8ea24c45d3ab4
addrick <asantos@wpi.edu>
2025-07-15 19:40:17 -0400

    not a fan of gradio, will find another solution

--- src/basic_webui.py
+++ src/basic_webui.py
@@ -1,5 +1,3 @@
-import gradio as gr
-
 from src.chat_system import ChatSystem
 
 
@@ -11,22 +9,18 @@ class WebUI:
         webui.launch()
 
     def create_interface(self):
-        with gr.Blocks(analytics_enabled=False) as demo:
-            # Define input and output components
-            persona_name = gr.Textbox(label="Persona Name")
-            message_in = gr.Textbox(label="Input")
-            message_out = gr.Textbox(label="Output", interactive=False)
-
-            self.setup_event_handlers(persona_name, message_in, message_out)
-        return demo
+        pass
 
 
     def setup_event_handlers(self, persona_name, message_in, message_out):
-        message_in.submit(self.handle_submission, inputs=[persona_name, message_in], outputs=message_out)
-
+        pass
 
     async def handle_submission(self, persona_name, message_in):
         # Use your existing function to process the input
         response = await self.bot.generate_response(persona_name=persona_name, message=message_in, context=[])
         return response
+
+    @classmethod
+    def launch(cls):
+        pass
 
